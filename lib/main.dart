18ab57370bb2b61d78bcd1f1af117cad7f57335b
ERIC <101303830+ElectroZombie@users.noreply.github.com>
2024-01-19 17:09:51 -0500

    actualizados los direccionamientos

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:trip_control_app/frames/calculator.dart';
+import 'package:trip_control_app/frames/principal.dart';
+import 'package:trip_control_app/frames/trip_control.dart';
+import 'package:trip_control_app/frames/trip_data.dart';
+import 'package:trip_control_app/frames/trip_list.dart';
+import 'package:trip_control_app/models/trip_model.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +15,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
       ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const Principal(),
+        '/calculator': (BuildContext context) => const Calculator(),
+        '/trip_control': (BuildContext context) => const TripControl(TripModel),
+        '/trip_data': (BuildContext context) => const TripData(),
+        'trip_list': (BuildContext context) => const TripList()
+      },
     );
   }
 }
