dd68515588ac3592bd8ef1517d7c6ebb3fbb7e5c
ERIC <101303830+ElectroZombie@users.noreply.github.com>
2024-03-09 10:27:12 -0500

    Cerradas las constantes

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,13 @@ import 'package:trip_control_app/frames/principal.dart';
 import 'package:trip_control_app/frames/trip_control.dart';
 import 'package:trip_control_app/frames/trip_data.dart';
 import 'package:trip_control_app/frames/trip_list.dart';
-import 'package:trip_control_app/utils/tuple.dart';
+import 'package:trip_control_app/models/trip_model.dart';
 
 // ignore: depend_on_referenced_packages
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main() async {
-  runApp(const MainApp());
+  runApp(MainApp());
 
   if (Platform.isLinux) {
     sqfliteFfiInit();
@@ -21,7 +21,7 @@ void main() async {
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +31,7 @@ class MainApp extends StatelessWidget {
       routes: {
         '/': (context) => Principal(),
         '/calculator': (BuildContext context) => const Calculator(),
-        '/trip_control': (BuildContext context) => const TripControl(Tuple),
+        '/trip_control': (BuildContext context) => TripControl(),
         '/trip_data': (BuildContext context) => const TripData(),
         '/trip_list': (BuildContext context) => const TripList()
       },
