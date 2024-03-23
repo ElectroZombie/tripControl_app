029206cd4c3bc54b561992c17e866cfa98613c8a
ERIC <101303830+ElectroZombie@users.noreply.github.com>
2024-03-22 22:28:36 -0400

    Actualizaciones del frame de currentTrip

--- lib/widgets/current_trip_widget.dart
+++ lib/widgets/current_trip_widget.dart
@@ -38,6 +38,18 @@ Widget currentTripWidget(TripModel trip, context) {
         keyboardType: TextInputType.text,
         style: const TextStyle(fontSize: 14),
       ),
+      TextFormField(
+        controller: precioM1,
+        maxLength: 20,
+        keyboardType: TextInputType.text,
+        style: const TextStyle(fontSize: 14),
+      ),
+      TextFormField(
+        controller: precioM2,
+        maxLength: 20,
+        keyboardType: TextInputType.text,
+        style: const TextStyle(fontSize: 14),
+      ),
       FutureBuilder(
         future: getCompras(trip.tripID),
         initialData: null,
@@ -51,15 +63,76 @@ Widget currentTripWidget(TripModel trip, context) {
             itemBuilder: (BuildContext context, int i) {
               return ListTile(
                 title: Text(snapshot.data[i]['nombre_compra']),
+                leading: IconButton(
+                    onPressed: () => actualizarCompra(snapshot.data[i]),
+                    icon: Icon(Icons.ac_unit)),
+                trailing: IconButton(
+                  onPressed: () => eliminarCompra(snapshot.data[i]),
+                  icon: Icon(Icons.delete),
+                ),
               );
             },
           );
         },
       ),
+      TextButton(
+          onPressed: () => agregarCompra(), child: Text("Agregar compra")),
+      FutureBuilder(
+        future: getGastos(trip.tripID),
+        initialData: null,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Text("Sin gastos");
+          } else {
+            return ListView.builder(
+              shrinkWrap: true,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, i) {
+                return ListTile(
+                  title: Text(snapshot.data![i].gastoDescripcion),
+                  leading: IconButton(
+                      onPressed: () => actualizarGasto(snapshot.data![i].id),
+                      icon: Icon(Icons.ac_unit)),
+                  trailing: IconButton(
+                    onPressed: () => eliminarGasto(snapshot.data![i].id),
+                    icon: Icon(Icons.delete),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+      TextButton(onPressed: () => agregarGasto(), child: Text("Agregar gasto")),
+      Text(gastoT.toString()),
+      Text(gastoCompras.toString()),
+      Text(gastoComprasKilo.toString()),
+      Text(gastoOtros.toString()),
+      Text(gananciaReal.toString()),
+      Text(gananciaKilo.toString()),
+      Text(rentabilidadR.toString()),
+      Text(rentabilidadKilo.toString()),
+      Text(rentabilidadPorcentual.toString()),
     ],
   );
 }
 
+void actualizarCompra(compra) {}
+
+void agregarCompra() {}
+
+void eliminarCompra(compra) {}
+
+void actualizarGasto(gasto) {}
+
+void agregarGasto() {}
+
+void eliminarGasto(gasto) {}
+
 Future<List<CompraModel>> getCompras(int id) async {
   return DB.getComprasTrip(id);
+}
+
+Future<List<GastoModel>> getGastos(int id) async {
+  return DB.getGastosTrip(id);
 }
