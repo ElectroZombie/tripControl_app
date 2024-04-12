d930ef26bcb2e23e9ab3660d579ef4de18676936
ERIC <101303830+ElectroZombie@users.noreply.github.com>
2024-04-12 10:03:11 -0400

    Arreglado el roll del tripList

--- lib/frames/trip_list.dart
+++ lib/frames/trip_list.dart
@@ -20,71 +20,72 @@ class TripListState extends State<TripList> {
     return Stack(
       children: [
         gradient(),
-        FutureBuilder(
-          future: loadTrips(),
-          initialData: null,
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (!snapshot.hasData || snapshot.data.length == 0) {
-              return Column(
-                children: [
-                  const Text("No hay viajes disponibles"),
-                  TextButton(
-                      onPressed: () =>
-                          {Navigator.pushNamed(context, '/trip_control')},
-                      child: const Text("Crear viaje"))
-                ],
-              );
-            } else {
-              return SingleChildScrollView(
-                  child: Center(
-                      child: SizedBox(
-                          width: (MediaQuery.of(context).size.width * 7) / 10,
-                          child: Column(
-                            children: [
-                              ListView.builder(
-                                shrinkWrap: true,
-                                itemCount: snapshot.data.length,
-                                itemBuilder: (context, r) {
-                                  TripModel viaje = snapshot.data[r];
-                                  return Column(
-                                    children: [
-                                      ListTile(
-                                          tileColor: const Color.fromARGB(
-                                              104, 105, 89, 112),
-                                          shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(30)),
-                                          title: Text(
-                                            "Nombre del viaje: ${viaje.tripName}"
-                                            "\n Pais del viaje: ${viaje.nombrePais}"
-                                            "\n Fechas del viaje: ${viaje.fechaInicioViaje} - ${viaje.fechaFinalViaje}",
-                                            style: TextStyle(fontSize: 16),
-                                          ),
-                                          leading: IconButton(
-                                              onPressed: () => {
-                                                    Navigator.pushNamed(
-                                                        context, '/trip_data',
-                                                        arguments: snapshot
-                                                            .data[r].tripID)
-                                                  },
-                                              hoverColor: Color.fromARGB(
-                                                  143, 14, 100, 139),
-                                              tooltip: "Ver informacion",
-                                              icon: const Icon(Icons
-                                                  .arrow_circle_right_outlined))),
-                                      const SizedBox(
-                                        height: 5,
-                                      ),
-                                    ],
-                                  );
-                                },
-                              ),
-                              nuevoViaje(snapshot, context),
-                            ],
-                          ))));
-            }
-          },
-        ),
+        SingleChildScrollView(
+          child: FutureBuilder(
+            future: loadTrips(),
+            initialData: null,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (!snapshot.hasData || snapshot.data.length == 0) {
+                return Column(
+                  children: [
+                    const Text("No hay viajes disponibles"),
+                    TextButton(
+                        onPressed: () =>
+                            {Navigator.pushNamed(context, '/trip_control')},
+                        child: const Text("Crear viaje"))
+                  ],
+                );
+              } else {
+                return Center(
+                    child: SizedBox(
+                        width: (MediaQuery.of(context).size.width * 7) / 10,
+                        child: Column(
+                          children: [
+                            ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: snapshot.data.length,
+                              itemBuilder: (context, r) {
+                                TripModel viaje = snapshot.data[r];
+                                return Column(
+                                  children: [
+                                    ListTile(
+                                        tileColor:
+                                            Color.fromARGB(255, 105, 89, 112),
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(30)),
+                                        title: Text(
+                                          "Nombre del viaje: ${viaje.tripName}"
+                                          "\n Pais del viaje: ${viaje.nombrePais}"
+                                          "\n Fechas del viaje: ${viaje.fechaInicioViaje} - ${viaje.fechaFinalViaje}",
+                                          style: TextStyle(fontSize: 16),
+                                        ),
+                                        leading: IconButton(
+                                            onPressed: () => {
+                                                  Navigator.pushNamed(
+                                                      context, '/trip_data',
+                                                      arguments: snapshot
+                                                          .data[r].tripID)
+                                                },
+                                            hoverColor: Color.fromARGB(
+                                                202, 14, 99, 139),
+                                            tooltip: "Ver informacion",
+                                            icon: const Icon(Icons
+                                                .arrow_circle_right_outlined))),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                  ],
+                                );
+                              },
+                            ),
+                            nuevoViaje(snapshot, context),
+                          ],
+                        )));
+              }
+            },
+          ),
+        )
       ],
     );
   }
